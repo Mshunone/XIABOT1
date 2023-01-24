b349006caab89f50e6ca513ce59a4b47e453df50
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-01-24 09:49:00 +0700

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f0b1c19aca4215ffb4ac7.jpg', m, { packname: "wa.me/6287855066799", author: "мυʀѕι∂ вσт-χмℓ\n> Instagram : @mursid.st" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f0b1c19aca4215ffb4ac7.jpg', m, { packname: "wa.me/6283111136642", author: "Saxia Botz\nig @m.rxcl_" })
 }
 
 handler.customPrefix = /^(ok|okey|otey|okeh)$/i
