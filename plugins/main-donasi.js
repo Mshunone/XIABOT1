af65bab52389ee2ecf7b65c8e0aaa7f3b45a562f
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-01-24 09:39:27 +0700

    Update main-donasi.js

--- plugins/main-donasi.js
+++ plugins/main-donasi.js
@@ -1,17 +1,15 @@
-const qrmursid = "https://telegra.ph/file/c8a8cabe89cda01e0fec9.jpg"
+const qrsaxia = "https://telegra.ph/file/c8a8cabe89cda01e0fec9.jpg"
 
-let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrmursid, `
-┏──「 *Donasi • E-money* 」─⬣
-│ • Gopay : 088233832771
-│ • Dana : 088233832771
-│ • Smartfren : 088233832771
-│ • Tri : 089674452300
+let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrsaxia, `
+┏──「 *Donasi Orang Baik* 」─⬣
+│ • Seikhlas nya saja
+│ • All Peyment
 ┗────────⬣
 ┏──「 *NOTE* 」─⬣
-│ > Ingin donasi? wa.me/6288233832771
-│ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
-│ _24jam tanpa ada kendala sama sekali_
+│ SeikhlaSny aja klo bisa 
+│ Klo bisa 10k :v
+│ Pencet Doang di perkaos owner
+│ pencet donasi doang ga donasi
 ┗────────⬣
 `.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
 handler.help = ['donasi']
