d766db67e87fcc8c0a1d8f5e9d2cbfa71863904b
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-01-24 08:36:38 +0700

    Update _autoresponlink.js

--- plugins/_autoresponlink.js
+++ plugins/_autoresponlink.js
@@ -5,9 +5,11 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
     let teks = `Invite Group
-• 15 Day / Rp 5k
-• 30 Day / Rp 10k
-Jika berminat hubungi: @${global.owner[0]} untuk order:)
+⬣ 1 Bulan / Rp 10k
+⬣ 1 Tahun / Rp 100k
+[•] Info Lebih Lanjut Chek Katalog bot [•]
+
+⻝ Jika berminat hubungi: @${global.owner[0]} untuk order:)
 `
     this.reply(m.chat, teks, m)
     const data = global.owner.filter(([id, isCreator]) => id && isCreator)
