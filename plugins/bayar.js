196370d9963b013a6c1469adb1408f28907349ac
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-01-24 08:50:32 +0700

    Update bayar.js

--- plugins/bayar.js
+++ plugins/bayar.js
@@ -1,7 +1,7 @@
 const kontol = "https://telegra.ph/file/f4b21013c0b52e0ef1b02.jpg"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, kontol, `
-Silahkan scan barcode di atas untuk melakukan transaksi, wajin menyertakan bukti transfer
+Silahkan scan barcode di atas untuk melakukan transaksi, wajib menyertakan bukti transfer
 `.trim(), wm, 'Owner', usedPrefix + 'owner', m) // Tambah sendiri kalo mau
 handler.help = ['bayar']
 handler.tags = ['main', 'utama']
